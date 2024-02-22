295a7b0ad78fcda3c26e08d37b6c207b195dd5ba
NRPonugoti <87998959+NRPonugoti@users.noreply.github.com>
2024-02-22 21:02:36 +0200

    Create for.sh

---
+++ shell_Scripts/for.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+for i in {1..20}
+do 
+    echo "numbers  - ${i}"
+
+done
+
+=====================================
+
+#!/bin/bash 
+
+for i in 1 2 3 4 
+do 
+   echo "number -${i}"
+done 
+
+==================================
+#!/bin/bash
+for i in nare  ponu su 
+do 
+   echo "string - ${i}"
+done 
