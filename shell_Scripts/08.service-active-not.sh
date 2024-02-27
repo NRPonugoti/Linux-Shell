e6a81603dbbbb7cd1ea84256278f11e5105eda39
NRPonugoti <87998959+NRPonugoti@users.noreply.github.com>
2024-02-27 22:34:02 +0200

    Update 08.service-active-not.sh

--- shell_Scripts/08.service-active-not.sh
+++ shell_Scripts/08.service-active-not.sh
@@ -1,1 +1,12 @@
+#!/bin/bash 
+echo "=====status check docker services==============="
+status="`systemctl status docker | awk 'NR==3 {print}'|cut -d ':' -f 2 | cut -d '(' -f 1`"
+#NR means number of records 
 
+echo $status
+if[ $status =="active" ];
+then 
+  echo "service is running fine ...."
+else 
+  echo "service is not running "
+fi
