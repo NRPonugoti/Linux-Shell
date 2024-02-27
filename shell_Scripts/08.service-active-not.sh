7f5d6a21d043f7278d0301e6a8f0aa5a5eb6e620
NRPonugoti <87998959+NRPonugoti@users.noreply.github.com>
2024-02-27 22:36:36 +0200

    Update 08.service-active-not.sh

--- shell_Scripts/08.service-active-not.sh
+++ shell_Scripts/08.service-active-not.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 echo "=====status check docker services==============="
 status="`systemctl status docker | awk 'NR==3 {print}'|cut -d ':' -f 2 | cut -d '(' -f 1`"
+# this is tick symbol `` , we write anything inside this ``this will be assumed as a command and the complete output of this command
 #NR means number of records 
 
 echo $status
-if[ $status =="active" ];
+if[ $status ="active" ];
 then 
   echo "service is running fine ...."
 else 
